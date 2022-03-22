70de8e7811a39b05ed0e3a9a081d12c3e8570180
erhic <eric.gichovi@student.moringaschool.com>
2022-03-22 17:20:08 +0300

    updated views on resizing image

--- allprojects/models.py
+++ allprojects/models.py
@@ -17,22 +17,22 @@ from  PIL import Image
 
 class Profile(models.Model):
     user= models.OneToOneField(User, on_delete=models.CASCADE)
-    image= CloudinaryField("image",default='default.png',)
+    image= models.ImageField("image",default='default.png',)
     contact=models.CharField(blank=True,max_length=50)
     
     def __str__(self):
         return f'{self.user.username}Profile'
     
     
-    # def save(self,**kwarg):
-    #     super().save()
-    #     #  the below variable will store  every instance of the image before risizing
-    #     img= Image.open(self.image.path)
+    def save(self,**kwarg):
+        super().save()
+        # #  the below variable will store  every instance of the image before risizing
+        # img= Image.open(self.image.path)
         
-    #     if img.height>300 or img.width>300:
-    #         output_size=(300,300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
+        # if img.height>300 or img.width>300:
+        #     output_size=(300,300)
+        #     img.thumbnail(output_size)
+        #     img.save(self.image.path)
         
 
 
