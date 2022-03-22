c55f76f629f7096bb70d92f2e6a53f3ad4363101
erhic <eric.gichovi@student.moringaschool.com>
2022-03-22 17:07:05 +0300

    updated views on resizing image

--- allprojects/models.py
+++ allprojects/models.py
@@ -24,15 +24,15 @@ class Profile(models.Model):
         return f'{self.user.username}Profile'
     
     
-    def save(self,**kwarg):
-        super().save()
-        #  the below variable will store  every instance of the image before risizing
-        img= Image.open(self.image.path)
+    # def save(self,**kwarg):
+    #     super().save()
+    #     #  the below variable will store  every instance of the image before risizing
+    #     img= Image.open(self.image.path)
         
-        if img.height>300 or img.width>300:
-            output_size=(300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height>300 or img.width>300:
+    #         output_size=(300,300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
         
 
 
